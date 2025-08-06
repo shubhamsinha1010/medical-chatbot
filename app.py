036faf65015eea86570f67e4ab270cefcc2335f1
shubhamsinha1010 <sinhashubham1911@gmail.com>
2025-08-06 12:02:41 +0530

    fix: Lint checks

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
-from src.prompt import *
+from src.prompt import system_prompt
 import os
 
 app = Flask(__name__)
--- store_index.py
+++ store_index.py
@@ -1,4 +1,5 @@
-from src.helper import load_pdf_file, text_split, download_hugging_face_embeddings
+from src.helper import (load_pdf_file, text_split,
+                        download_hugging_face_embeddings)
 from pinecone.grpc import PineconeGRPC as Pinecone
 from pinecone import ServerlessSpec
 from langchain_pinecone import PineconeVectorStore
