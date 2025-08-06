c5e28c0092be1d34034e5b6c77ec940bb3bdecaf
shubhamsinha1010 <sinhashubham1911@gmail.com>
2025-08-06 11:36:46 +0530

    feat: Add lint check job

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, request
 from src.helper import download_hugging_face_embeddings
 from langchain_pinecone import PineconeVectorStore
-from langchain_groq import ChatGroq  # Changed from OpenAI to Groq
+from langchain_groq import ChatGroq
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
